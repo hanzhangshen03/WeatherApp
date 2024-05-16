21756118b0303243edb76b2342d27b3bb94a58bc
alice <as3520@cam.ac.uk>
2024-05-16 15:56:45 +0100

    Updated BeachSelection to be an even better hello world, with screen split in half

--- app/(tabs)/beachSelection.tsx
+++ app/(tabs)/beachSelection.tsx
@@ -3,16 +3,32 @@ import {StyleSheet, TouchableOpacity, Text, View} from 'react-native';
 
 export default function BeachSelection () {
   return (
-  <View>
+  <div style={styles.divthing}>
+  <View style={styles.hello}>
   <Text style={styles.hello}>Hello World :3</Text>
   </View>
+  <View style={styles.hello2}>
+  <Text style={styles.hello2}>Hello World 2 :3</Text>
+  </View>
+  </div>
   );
 }
 
 const styles = StyleSheet.create({
+  divthing: {
+    height: '100%'
+  },
   hello: {
     alignItems: 'center',
     color: 'pink',
-    backgroundColor: 'green'
+    backgroundColor: 'green',
+    height: '60%'
+  },
+
+  hello2: {
+    alignItems: 'center',
+    color: 'pink',
+    backgroundColor: 'cyan',
+    height: '40%'
   }
 })
