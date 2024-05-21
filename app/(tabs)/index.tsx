899cbd41b49b7ede77ad810e533a60ac815df4a6
hanzhangshen03 <shenhanzhang@outlook.com>
2024-05-21 12:00:28 +0100

    Revert "A"
    This reverts commit d5ca29de7faa15c031687a470d9b76385ae106fb.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,73 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import React from 'react';
+import { Image, StyleSheet, View, ImageBackground } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import FrogIcon from '@/components/initial_setup/FrogIcon'; // Import the FrogIcon component
+import { DateTime } from '@/components/initial_setup/DateTimeComponent';
+import SearchBox from '@/components/initial_setup/SearchBox';
+import background from '@/assets/images/background.avif';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.page}>
+
+      <ScrollView style={styles.scroll}>
+
+        {/* Frog icon */}
+        <View style={styles.frogIcon}>
+          <FrogIcon />
+        </View>
+
+        {/* searchbox component */}
+        <View style={styles.searchBox}>
+          <SearchBox />
+        </View>
+
+        {/* date time component */}
+        <View style={styles.dateTime}>
+          <DateTime textColor={'white'} />
+        </View>
+      </ScrollView>
+
+      {/* background image */}
+      <ImageBackground source={background} style={[styles.background, { zIndex: -1 }]} />
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  dateTime: {
     alignItems: 'center',
-    gap: 8,
+    height: '30%',
+    paddingTop: 20
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  searchBox: {
+    alignItems: 'center'
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
+  scroll: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'transparent'
+  },
+  imagebox: {
+    height: '30%',
+    width: '100%'
+  },
+  frogIcon: {
     position: 'absolute',
+    top: 10,
+    right: 10,
+    zIndex: 1,
   },
+  page: {
+    height: 600
+  },
+  background: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  }
 });
